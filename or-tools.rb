6dd39fb9aa360f6d31374a27f895c1173f7e16d1
Corentin Le Molgat <corentinl@google.com>
2018-04-16 15:41:41 +0200

    Fix brew audit errors

--- or-tools.rb
+++ or-tools.rb
@@ -1,20 +1,18 @@
 class OrTools < Formula
   desc "Google's Operations Research tools"
   homepage "https://developers.google.com/optimization/"
-  head 'https://github.com/google/or-tools.git'
-
   url "https://github.com/google/or-tools.git",
       :tag => "v6.7.1",
       :revision => "2e4c5b3617c433ab5ccdad6f5f5f7ccaa82adda7"
+  head "https://github.com/google/or-tools.git"
 
-  depends_on 'cmake' => :build
-  depends_on 'coin-or-tools/coinor/cbc'
-  depends_on 'gflags'
-  depends_on 'glog'
-  depends_on 'protobuf'
-  depends_on 'pkg-config' => :build
-  depends_on 'libtool' => :build
-  depends_on 'scip' => :optional
+  depends_on "cmake" => :build
+  depends_on "libtool" => :build
+  depends_on "pkg-config" => :build
+  depends_on "coin-or-tools/coinor/cbc"
+  depends_on "gflags"
+  depends_on "glog"
+  depends_on "protobuf"
 
   needs :cxx11
 
@@ -25,10 +23,10 @@ class OrTools < Formula
     system "make", "cc"
     system "make", "PREFIX=#{prefix}", "install"
     # CMake
-    #mkdir "build" do
-    #  system "cmake", "..", "-DBUILD_SHARED_LIBS=ON", *std_cmake_args
-    #  system "make", "install"
-    #end
+    # mkdir "build" do
+    #   system "cmake", "..", "-DBUILD_SHARED_LIBS=ON", *std_cmake_args
+    #   system "make", "install"
+    # end
 
     # Produce pkg-config file under cmake/make
     (lib/"pkgconfig/libortools.pc").write <<~EOS
