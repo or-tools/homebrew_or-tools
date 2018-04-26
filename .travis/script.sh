75d7cd24bae1cee0d74d0333f1c40f5d00faae95
Corentin Le Molgat <corentinl@google.com>
2018-04-26 14:31:06 +0200

    Travis CI update
    - add brew info
    - add brew audit --online

--- .travis/script.sh
+++ .travis/script.sh
@@ -3,5 +3,7 @@ set -e
 
 set -x
 brew install --HEAD --build-from-source --verbose ./or-tools.rb
+brew info ortools
 brew list -v ortools
-brew audit --new-formula --strict or-tools
+
+brew audit --strict --online or-tools
