fd9b10aed99f4c640208d584886c57ac8fbb46b1
Corentin Le Molgat <corentinl@google.com>
2019-03-29 16:21:24 +0100

    travis up

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 set -ex
+# https://github.com/Homebrew/brew/issues/1742#issuecomment-277308817
+rm -f /usr/local/include/c++
 
 sw_vers
 
-# https://github.com/Homebrew/brew/issues/1742#issuecomment-277308817
-rm -f /usr/local/include/c++
+brew --version
 
 brew update
 
