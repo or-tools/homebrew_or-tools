880b3cf174abc0539dc270f92c1183863dd1c0e8
Corentin Le Molgat <corentinl@google.com>
2019-03-29 16:18:46 +0100

    Travis update

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 set -ex
 
+sw_vers
+
 # https://github.com/Homebrew/brew/issues/1742#issuecomment-277308817
 rm -f /usr/local/include/c++
 
 brew update
+
+# see https://github.com/travis-ci/travis-ci/issues/10275
+brew install gcc || brew link --overwrite gcc
+brew install make ccache
 
 # list all installed taps.
 brew tap
