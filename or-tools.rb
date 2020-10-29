485dd2458d7945ad3faff991c19ec69c42848740
Mizux Seiha <mizux.dev@gmail.com>
2020-10-29 09:37:03 +0100

    Update Formula

--- or-tools.rb
+++ or-tools.rb
@@ -4,7 +4,7 @@ class OrTools < Formula
   url "https://github.com/google/or-tools/archive/v8.0.tar.gz"
   sha256 "ac01d7ebde157daaeb0e21ce54923a48e4f1d21faebd0b08a54979f150f909ee"
   license "Apache-2.0"
-  head "https://github.com/google/or-tools/archive/master.zip"
+  head "https://github.com/google/or-tools.git"
 
   bottle do
     cellar :any
@@ -26,29 +26,36 @@ class OrTools < Formula
 
   def install
     # CMake based build
-    system "cmake", "-S.", "-Bbuild", *std_cmake_args, "-DUSE_SCIP=OFF", "-DBUILD_SAMPLES=OFF", "-DBUILD_EXAMPLES=OFF"
+    system "cmake", "-S.", "-Bbuild", *std_cmake_args,
+           "-DUSE_SCIP=OFF", "-DBUILD_SAMPLES=OFF", "-DBUILD_EXAMPLES=OFF"
     system "cmake", "--build", "build", "-v"
     system "cmake", "--build", "build", "--target", "install"
+    pkgshare.install "ortools/linear_solver/samples/simple_lp_program.cc"
+    pkgshare.install "ortools/constraint_solver/samples/simple_routing_program.cc"
+    pkgshare.install "ortools/sat/samples/simple_sat_program.cc"
   end
 
   test do
     # Linear Solver & Glop Solver
-    cp pkgshare/"ortools/linear_solver/samples/simple_lp_program.cc", testpath
-    system ENV.cxx, "-std=c++17", "simple_lp_program.cpp",
+    system ENV.cxx, "-std=c++17",
            "-I#{include}", "-L#{lib}", "-lortools",
-           "-o", "simple_lp_program"
+           "-L#{Formula["gflags"].opt_lib}", "-lgflags",
+           "-L#{Formula["glog"].opt_lib}", "-lglog",
+           pkgshare/"simple_lp_program.cc", "-o", "simple_lp_program"
     system "./simple_lp_program"
     # Routing Solver
-    cp pkgshare/"ortools/constraint_solver/samples/simple_routing_program.cc", testpath
-    system ENV.cxx, "-std=c++17", "simple_routing_program.cpp",
+    system ENV.cxx, "-std=c++17",
            "-I#{include}", "-L#{lib}", "-lortools",
-           "-o", "simple_routing_program"
+           "-L#{Formula["gflags"].opt_lib}", "-lgflags",
+           "-L#{Formula["glog"].opt_lib}", "-lglog",
+           pkgshare/"simple_routing_program.cc", "-o", "simple_routing_program"
     system "./simple_routing_program"
     # Sat Solver
-    cp pkgshare/"ortools/constraint_solver/samples/simple_sat_program.cc", testpath
-    system ENV.cxx, "-std=c++17", "simple_sat_program.cpp",
+    system ENV.cxx, "-std=c++17",
            "-I#{include}", "-L#{lib}", "-lortools",
-           "-o", "simple_sat_program"
+           "-L#{Formula["gflags"].opt_lib}", "-lgflags",
+           "-L#{Formula["glog"].opt_lib}", "-lglog",
+           pkgshare/"simple_sat_program.cc", "-o", "simple_sat_program"
     system "./simple_sat_program"
   end
 end
