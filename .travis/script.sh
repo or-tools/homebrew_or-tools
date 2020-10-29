41a4826ddf24ecc4a4fd145fbf26c7b0fbc8e286
Mizux Seiha <mizux.dev@gmail.com>
2020-10-29 09:36:40 +0100

    Remove custom abseil.rb
    * Have been added to homebrew-core
      see: https://github.com/Homebrew/homebrew-core/pull/62803

--- .travis/script.sh
+++ .travis/script.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-brew install --build-from-source --verbose ./abseil.rb
-
 brew install --HEAD --only-dependencies --verbose ./or-tools.rb
 brew install --HEAD --build-from-source --verbose ./or-tools.rb
 
